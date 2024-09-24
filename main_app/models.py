31752692bcfe85f22ac4d657035bbfeabe9bf47b
Nani1345 <xuna1026000@email.com>
2024-09-24 12:05:13 -0700

    creates detail function

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from datetime import date
 
+AGE = (
+  ('1', '0-1.5'),
+  ('2', '1.5-3'),
+  ('3', '3-5'),
+  ('4', '5-7'),
+  ('5', '7-10')
+)
 # Create your models here.
+class Activity(models.Model):
+  age = models.CharField(
+    max_length=1,
+    choices=AGE,
+    default=AGE[0][0]
+  )
+  name = models.CharField(max_length=100)
+  time = models.DateField('Activity Date')
+  location = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  
+  def __str__(self):
+    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Activity
 
 
 
-class Activity:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, age, name, time, location, description):
-    self.age = age
-    self.name = name
-    self.time = time
-    self.location = location
-    self.description = description
+# class Activity:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, age, name, time, location, description):
+#     self.age = age
+#     self.name = name
+#     self.time = time
+#     self.location = location
+#     self.description = description
 
-activities = [
-  Activity('1.5-3', 'Story time', '9/24/2024', 'downtown park', 'funny'),
-  Activity('0-1.5', 'play time', '9/26/2024','community center', 'funny'),
-]
+# activities = [
+#   Activity(age='1.5-3',name='Story time', time='9/24/2024', location='downtown park', description='funny'),
+#   Activity('0-1.5', 'play time', '9/26/2024','community center', 'funny'),
+# ]
 
 # Create your views here.
 def home(request):
@@ -24,4 +24,9 @@ def about(request):
   return render(request, 'about.html')
 
 def activity_index(request):
+  activities = Activity.objects.all()
   return render(request, 'activities/index.html', { 'activities': activities })
+
+def activity_detail(request, activity_id):
+  activity = Activity.objects.get(id=activity_id)
+  return render(request, 'activities/detail.html', { 'activity': activity})
