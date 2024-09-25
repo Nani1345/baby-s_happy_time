c289665dad03f1f814cd244315861afba55006b2
Nani1345 <xuna1026000@email.com>
2024-09-25 11:20:16 -0700

    add authentication

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('activities/', views.activity_index, name='activity-index'),
   path('activities/<int:activity_id>/', views.activity_detail, name='activity-detail'),
@@ -11,4 +11,5 @@ urlpatterns = [
   path('activities/<int:pk>/update/', views.ActivityUpdate.as_view(), name='activity-update'),
   path('activities/<int:pk>/delete/', views.ActivityDelete.as_view(), name='activity-delete'),
   path('add_activity/<int:activity_id>/', views.add_my_activities, name='add-my-activities'),
+  path('accounts/signup/', views.signup, name='signup'),
 ]
